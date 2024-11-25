aa120eadaf4ff9e2fbb2226509be8a9cc196bc7f
Pavel <pamitrovel@gmail.com>
2024-11-25 22:01:27 +0300

    refactor generateBelarusMobilePhone: add String countryCode

--- src/main/java/by/fixprice/utils/GenerationDataUtil.java
+++ src/main/java/by/fixprice/utils/GenerationDataUtil.java
@@ -8,23 +8,24 @@ import java.util.Random;
 public class GenerationDataUtil {
     public static Faker faker = new Faker();
 
-    public static String generateEmail(){
+    public static String generateEmail() {
         return faker.internet().emailAddress();
     }
 
-    public static String generatePassword(){
+    public static String generatePassword() {
         return faker.internet().password();
     }
 
-    public static String generateBelarusMobilePhone(){
+    public static String generateBelarusMobilePhone() {
         Random random = new Random();
+        String countryCode = "+375";
         int[] mobileCodesBelarus = {25, 29, 33, 44};
         int randomIndex = random.nextInt(mobileCodesBelarus.length);
         int mobileCodeBelarus = mobileCodesBelarus[randomIndex];
-        return String.format(mobileCodeBelarus + RandomStringUtils.randomNumeric(7));
+        return String.format(countryCode + mobileCodeBelarus + RandomStringUtils.randomNumeric(7));
     }
 
-    public static String generateIncorrectLogin(){
-        return RandomStringUtils.randomAscii(4,14);
+    public static String generateIncorrectLogin() {
+        return RandomStringUtils.randomAscii(4, 14);
     }
 }
