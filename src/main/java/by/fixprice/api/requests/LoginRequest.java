00b0d3c58a5f46c0699c5d12492f9f77455768cd
Pavel <pamitrovel@gmail.com>
2024-11-25 21:06:59 +0300

    refactor LoginRequest: add initRequestSpecification, getBody, getBodyEmailNull, getBodyPhoneNull, getBodyAllNulls

--- src/main/java/by/fixprice/api/requests/LoginRequest.java
+++ src/main/java/by/fixprice/api/requests/LoginRequest.java
@@ -1,25 +1,67 @@
 package by.fixprice.api.requests;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.internal.RequestSpecificationImpl;
+import io.restassured.specification.RequestSpecification;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.openqa.selenium.devtools.v85.network.model.Request;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class LoginRequest {
-    public static final String LOGIN_URL = "https://api.fix-price.by/buyer/v2/auth/login";
-    public static final String BODY_NULL_ALL = "{\"password\":null,\"email\":null,\"phone\":null}";
-    public static final String BODY_EMPTY_ALL = "{\"password\":\"\",\"email\":\"\",\"phone\":\"\"}";
+    public static RequestSpecification requestSpecification;
+
+    public static void initRequestSpecification() {
+        requestSpecification = new RequestSpecBuilder()
+
+                .setBaseUri("https://api.fix-price.by/")
+                .setBasePath("/buyer/v2/auth/login")
+                .setContentType("application/json")
+                .addHeader("x-city", "14")
+                .addHeader("x-key", "65bffe42769ff379b3a7a953e0561fb2")
+                .build();
+    }
+
+    public static String getBody(String password, String email, String phone) {
+        return "{\n" +
+                "    \"password\": \"" + password + "\",\n" +
+                "    \"email\": \"" + email + "\",\n" +
+                "    \"phone\": \"" + phone + "\"\n" +
+                "}";
+    }
+
+    public static String getBodyEmailNull(String password, String phone) {
+        return "{\n" +
+                "    \"password\": \"" + password + "\",\n" +
+                "    \"email\": null,\n" +
+                "    \"phone\": \"" + phone + "\"\n" +
+                "}";
+    }
+
+    public static String getBodyPhoneNull(String password, String email) {
+        return "{\n" +
+                "    \"password\": \"" + password + "\",\n" +
+                "    \"email\": \"" + email + "\",\n" +
+                "    \"phone\": null\n" +
+                "}";
+    }
+
+//    public static String getBodyPasswordNull(String password, String phone) {}
+
+    public static String getBodyAllNulls() {
+        return "{\n" +
+                "    \"password\": null,\n" +
+                "    \"email\": null,\n" +
+                "    \"phone\": null\n" +
+                "}";
+    }
+
+
 
     private static String numbers = "0123456789";
     private static String gmailDomain = "@gmail.com";
 
-    public static Map<String, String> getHeaders() {
-        Map<String, String> headers = new HashMap<String, String>();
-        headers.put("Content-Type", "application/json");
-        headers.put("x-city", "14");
-        headers.put("x-key", "65bffe42769ff379b3a7a953e0561fb2");
-        return headers;
-    }
 
     public static String getRandomEmail() {
         String lowerCase = "abcdefghijklmnopqrstuvwxyz";
@@ -31,7 +73,7 @@ public class LoginRequest {
     }
 
     public static String getRandomPhone() {
-        String phoneNumber = "+37529" + RandomStringUtils.random(7, numbers);
+        String phoneNumber = "+37529" + RandomStringUtils.randomNumeric(7);
         return phoneNumber;
     }
 }
