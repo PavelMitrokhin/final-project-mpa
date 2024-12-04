6dba074bd5fe661129db86c53987704856f119ca
Pavel <pamitrovel@gmail.com>
2024-12-04 22:59:33 +0300

    add logger

--- src/test/java/by/fixprice/ui/BaseTest.java
+++ src/test/java/by/fixprice/ui/BaseTest.java
@@ -1,13 +1,18 @@
 package by.fixprice.ui;
 
 import by.fixprice.ui.driver.Driver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
 
 public class BaseTest {
+    private static final Logger logger = LogManager.getLogger();
 
     @BeforeEach
-    public void setUpHomePage() {
+    public void setUpHomePage(TestInfo testInfo) {
+        logger.info(testInfo.getDisplayName());
         Driver.getDriver().get("https://fix-price.by/");
     }
 
