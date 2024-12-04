88eb715a9737d0a2893f954d2d194e8eef8d95a2
Pavel <pamitrovel@gmail.com>
2024-12-04 23:00:20 +0300

    add LoginFormTest

--- src/test/java/by/fixprice/ui/LoginFormTest.java
+++ src/test/java/by/fixprice/ui/LoginFormTest.java
@@ -1,8 +1,26 @@
 package by.fixprice.ui;
 
+import by.fixprice.ui.pages.home.HomePage;
+import by.fixprice.ui.pages.home.forms.login.LoginForm;
+import by.fixprice.ui.pages.home.forms.login.LoginFormExpectations;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class LoginFormTest extends BaseTest{
+public class LoginFormTest extends BaseTest {
+    HomePage homePage = new HomePage();
+    LoginForm loginForm = new LoginForm();
+
     @Test
-    public void testLoginForm() {}
+    @DisplayName("Invalid phone user")
+    public void testLoginForm() {
+        homePage.openLoginForm();
+        loginForm.
+                clickLoginField().
+                sendLogin("292221122").
+                sendPassword("dfsdhfwkejfl123123").
+                clickAgreementCheckbox().
+                clickEnterLoginForm();
+        Assertions.assertEquals(LoginFormExpectations.LOGIN_FORM_TITLE, loginForm.getTitleLoginForm());
+    }
 }
