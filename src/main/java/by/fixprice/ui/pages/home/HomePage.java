40032aa53e52261b77128753b74d66466342cd6a
Pavel <pamitrovel@gmail.com>
2024-12-04 21:54:19 +0300

    add methods to interact with login form

--- src/main/java/by/fixprice/ui/pages/home/HomePage.java
+++ src/main/java/by/fixprice/ui/pages/home/HomePage.java
@@ -15,4 +15,39 @@ public class HomePage {
         driver.findElement(By.xpath(HomePageXpath.BUTTON_LOGIN_FORM_XPATH)).click();
         return this;
     }
+
+    public HomePage selectPhoneTabLoginForm() {
+        driver.findElement(By.xpath(HomePageXpath.BUTTON_PHONE_TAB_XPATH)).click();
+        return this;
+    }
+
+    public HomePage selectEmailTabLoginForm() {
+        driver.findElement(By.xpath(HomePageXpath.BUTTON_EMAIL_TAB_XPATH)).sendKeys();
+        return this;
+    }
+
+    public HomePage sendEmailOrPhoneLoginForm() {
+        driver.findElement(By.xpath(HomePageXpath.INPUT_PHONE_EMAIL_XPATH)).sendKeys();
+        return this;
+    }
+
+    public HomePage sendPasswordLoginForm() {
+        driver.findElement(By.xpath(HomePageXpath.INPUT_PASSWORD_XPATH)).sendKeys();
+        return this;
+    }
+
+    public HomePage clickCheckboxLoginForm() {
+        driver.findElement(By.xpath(HomePageXpath.CHECKBOX_AGREEMENT_XPATH)).click();
+        return this;
+    }
+
+    public HomePage clickEnterLoginForm() {
+        driver.findElement(By.xpath(HomePageXpath.BUTTON_ENTER_XPATH)).click();
+        return this;
+    }
+
+    public String getTitleLoginForm(){
+        return driver.findElement(By.xpath(HomePageXpath.LOGIN_FORM_TITLE_XPATH)).getText();
+    }
+
 }
