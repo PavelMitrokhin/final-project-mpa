3a1eab3b553c0e103b36ad98ad4bfb44cab07917
Pavel <pamitrovel@gmail.com>
2024-12-05 19:27:36 +0300

    overload generateBelarusMobilePhone

--- src/main/java/by/fixprice/utils/GenerationDataUtil.java
+++ src/main/java/by/fixprice/utils/GenerationDataUtil.java
@@ -16,12 +16,19 @@ public class GenerationDataUtil {
         return faker.internet().password(8,20, true, true);
     }
 
-    public static String generateBelarusMobilePhone() {
+    public static String generateBelarusMobilePhone(String countryCode) {
         Random random = new Random();
-        String countryCode = "+375";
         int[] mobileCodesBelarus = {25, 29, 33, 44};
         int randomIndex = random.nextInt(mobileCodesBelarus.length);
         int mobileCodeBelarus = mobileCodesBelarus[randomIndex];
         return String.format(countryCode + mobileCodeBelarus + RandomStringUtils.randomNumeric(7));
+    }
+
+    public static String generateBelarusMobilePhone() {
+        Random random = new Random();
+        int[] mobileCodesBelarus = {25, 29, 33, 44};
+        int randomIndex = random.nextInt(mobileCodesBelarus.length);
+        int mobileCodeBelarus = mobileCodesBelarus[randomIndex];
+        return String.format(mobileCodeBelarus + RandomStringUtils.randomNumeric(7));
     }
 }
