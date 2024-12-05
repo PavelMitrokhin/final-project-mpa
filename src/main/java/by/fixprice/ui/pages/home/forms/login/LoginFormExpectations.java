ab4214a064e13cad74dd2d41c42481200bee3775
Pavel <pamitrovel@gmail.com>
2024-12-05 20:58:56 +0300

    add INCORRECT_EMAIL_OR_PASSWORD

--- src/main/java/by/fixprice/ui/pages/home/forms/login/LoginFormExpectations.java
+++ src/main/java/by/fixprice/ui/pages/home/forms/login/LoginFormExpectations.java
@@ -2,4 +2,5 @@ package by.fixprice.ui.pages.home.forms.login;
 
 public class LoginFormExpectations {
     public static final String INVALID_LOGIN_OR_PASSWORD = "Неверный логин или пароль. Проверьте введённые данные и попробуйте снова. Осталось попыток: 4";
+    public static final String INCORRECT_EMAIL_OR_PASSWORD = "Укажите корректный email";
 }
