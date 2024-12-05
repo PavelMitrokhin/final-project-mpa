4ba0a288702f6746f4975d278e307cb6ae40e7ff
Pavel <pamitrovel@gmail.com>
2024-12-05 19:54:02 +0300

    refactor testInvalidPhoneUser

--- src/test/java/by/fixprice/ui/LoginFormTest.java
+++ src/test/java/by/fixprice/ui/LoginFormTest.java
@@ -1,27 +1,16 @@
 package by.fixprice.ui;
 
-import by.fixprice.ui.pages.home.HomePage;
-import by.fixprice.ui.pages.home.forms.login.LoginForm;
 import by.fixprice.ui.pages.home.forms.login.LoginFormExpectations;
-import by.fixprice.utils.UiUsers;
+import by.fixprice.ui.pages.home.forms.login.LoginFormSteps;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LoginFormTest extends BaseTest {
-    HomePage homePage = new HomePage();
-    LoginForm loginForm = new LoginForm();
 
     @Test
     @DisplayName("Invalid phone user")
     public void testInvalidPhoneUser() {
-        UiUsers user = new UiUsers();
-        homePage.openLoginForm();
-        loginForm.
-                sendLogin(user.getPhoneAndPasswordUser()).
-                sendPassword(user.getPhoneAndPasswordUser()).
-                clickAgreementCheckbox().
-                clickEnterLoginForm();
-        Assertions.assertEquals(LoginFormExpectations.LOGIN_FORM_TITLE, loginForm.getTitleLoginForm());
+        Assertions.assertEquals(LoginFormExpectations.LOGIN_FORM_TITLE, new LoginFormSteps().checkInvalidPhoneUser());
     }
 }
