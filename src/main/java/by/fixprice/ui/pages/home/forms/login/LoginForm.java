1d8504f667b0a0c1751adbc8a5e67f3743abcfca
Pavel <pamitrovel@gmail.com>
2024-12-05 22:55:38 +0300

    refactor sendLogin, sendPassword with logger

--- src/main/java/by/fixprice/ui/pages/home/forms/login/LoginForm.java
+++ src/main/java/by/fixprice/ui/pages/home/forms/login/LoginForm.java
@@ -2,11 +2,14 @@ package by.fixprice.ui.pages.home.forms.login;
 
 import by.fixprice.ui.driver.Driver;
 import by.fixprice.utils.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginForm {
     private WebDriver driver;
+    private static final Logger logger = LogManager.getLogger();
 
     public LoginForm() {
         driver = Driver.getDriver();
@@ -28,11 +31,13 @@ public class LoginForm {
     }
 
     public LoginForm sendLogin(User user) {
+        logger.info("Login: {}", user.getLogin());
         driver.findElement(By.xpath(LoginFormXpath.INPUT_LOGIN_XPATH)).sendKeys(user.getLogin());
         return this;
     }
 
     public LoginForm sendPassword(User user) {
+        logger.info("Password: {}", user.getPassword());
         driver.findElement(By.xpath(LoginFormXpath.INPUT_PASSWORD_XPATH)).sendKeys(user.getPassword());
         return this;
     }
