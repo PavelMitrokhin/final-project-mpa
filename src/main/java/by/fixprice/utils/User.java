63cf511c5c491f60aec85f9ff7d44693a7813eb4
Pavel <pamitrovel@gmail.com>
2024-12-05 19:31:07 +0300

    rebase utils, refactor User for UI and API, creat UiUsers

--- src/main/java/by/fixprice/utils/User.java
+++ src/main/java/by/fixprice/utils/User.java
@@ -1,31 +1,52 @@
 package by.fixprice.utils;
 
 public class User {
-    private String email;
-    private String phoneNumber;
     private String password;
+    private String email;
+    private String phone;
+    private String login;
 
-    public User(String password, String email, String phoneNumber) {
-        this.password = password;
-        this.email = email;
-        this.phoneNumber = phoneNumber;
+    public User(String password, String email, String phone) {
+        if (password != null) {
+            this.password = "\"" + password + "\"";
+        } else this.password = null;
 
+        if (email != null) {
+            this.email = "\"" + email + "\"";
+        } else this.email = null;
+
+        if (phone != null) {
+            this.phone = "\"" + phone + "\"";
+        } else this.phone = null;
     }
 
-    public User(String password, String email) {
+    public User(String login, String password) {
+        this.login = login;
         this.password = password;
-        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
+    public String getPhone() {
+        return phone;
     }
 
-    public String getPassword() {
-        return password;
+    public String getLogin() {
+        return login;
+    }
+
+    @Override
+    public String toString() {
+        return "ApiUser{" +
+                "password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
     }
 }
