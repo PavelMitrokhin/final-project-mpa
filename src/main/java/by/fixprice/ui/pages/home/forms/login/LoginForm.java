74b83f4aebd72dc5082e0073338f727039c1e678
Pavel <pamitrovel@gmail.com>
2024-12-07 18:23:57 +0300

    add methods and xpath for interacting with Register form, add loggers to all LoginForm's methods

--- src/main/java/by/fixprice/ui/pages/home/forms/login/LoginForm.java
+++ src/main/java/by/fixprice/ui/pages/home/forms/login/LoginForm.java
@@ -15,18 +15,19 @@ public class LoginForm {
         driver = Driver.getDriver();
     }
 
+    public String getLoginTitle() {
+        return driver.findElement(By.xpath(LoginFormXpath.LOGIN_TITLE_XPATH)).getText();
+    }
+
     public LoginForm selectPhoneTab() {
+        logger.info("Select phone tab");
         driver.findElement(By.xpath(LoginFormXpath.BUTTON_PHONE_TAB_XPATH)).click();
         return this;
     }
 
     public LoginForm selectEmailTab() {
+        logger.info("Select email tab");
         driver.findElement(By.xpath(LoginFormXpath.BUTTON_EMAIL_TAB_XPATH)).click();
-        return this;
-    }
-
-    public LoginForm clickLoginField() {
-        driver.findElement(By.xpath(LoginFormXpath.INPUT_LOGIN_XPATH)).click();
         return this;
     }
 
@@ -43,16 +44,65 @@ public class LoginForm {
     }
 
     public LoginForm clickAgreementCheckbox() {
+        logger.info("Click agreement checkbox");
         driver.findElement(By.xpath(LoginFormXpath.CHECKBOX_AGREEMENT_XPATH)).click();
         return this;
     }
 
     public LoginForm clickEnterLoginForm() {
+        logger.info("Click \'Enter\'");
         driver.findElement(By.xpath(LoginFormXpath.BUTTON_ENTER_XPATH)).click();
         return this;
     }
 
     public String getErrorInvalidLoginOrPassword() {
+        logger.info("Get error");
         return driver.findElement(By.xpath(LoginFormXpath.INVALID_LOGIN_OR_PASSWORD_XPATH)).getText();
+    }
+
+    public LoginForm clickRegisterButton() {
+        logger.info("Click register button");
+        driver.findElement(By.xpath(LoginFormXpath.BUTTON_REGISTER_XPATH)).click();
+        return this;
+    }
+
+    public LoginForm clickContinueRegisterButton() {
+        logger.info("Click continue register button");
+        driver.findElement(By.xpath(LoginFormXpath.BUTTON_CONTINUE_REGISTER_XPATH)).click();
+        return this;
+    }
+
+    public String getCodeSentTitle() {
+        logger.info("Get 'send code' title");
+        return driver.findElement(By.xpath(LoginFormXpath.CODE_IS_SENT_TITLE_XPATH)).getText();
+    }
+
+    public LoginForm changeRegisterPhone() {
+        logger.info("Change register phone number");
+        driver.findElement(By.xpath(LoginFormXpath.BUTTON_CHANGE_PHONE_XPATH)).click();
+        return this;
+    }
+
+    public LoginForm resendCode() {
+        logger.info("Resend code");
+        driver.findElement(By.xpath(LoginFormXpath.BUTTON_RESEND_CODE_XPATH)).click();
+        return this;
+    }
+
+    public LoginForm backToLogin() {
+        logger.info("Back to login");
+        driver.findElement(By.xpath(LoginFormXpath.BUTTON_BACK_TO_LOGIN_XPATH)).click();
+        return this;
+    }
+
+    public LoginForm clickSmsHelp() {
+        logger.info("SMS help");
+        driver.findElement(By.xpath(LoginFormXpath.BUTTON_SMS_HELP_XPATH)).click();
+        return this;
+    }
+
+    public String getSmsHelpTitle() {
+        logger.info("Get sms help title");
+        return driver.findElement(By.xpath(LoginFormXpath.SMS_HELP_TITLE_XPATH)).getText();
     }
 }
--- src/main/java/by/fixprice/ui/pages/home/forms/login/LoginFormXpath.java
+++ src/main/java/by/fixprice/ui/pages/home/forms/login/LoginFormXpath.java
@@ -3,11 +3,21 @@ package by.fixprice.ui.pages.home.forms.login;
 public class LoginFormXpath {
     public static final String BUTTON_PHONE_TAB_XPATH = "//*[@id=\"modal\"]/div/div/div[1]/button[1]";
     public static final String BUTTON_EMAIL_TAB_XPATH = "//*[@id=\"modal\"]/div/div/div[1]/button[2]";
-    public static final String INPUT_LOGIN_XPATH = "//*[@id=\"modal\"]/div/div/div[2]/div/input";
+    public static final String INPUT_LOGIN_XPATH = "//input[@class='input-text'][@type='text']";
     public static final String INPUT_PASSWORD_XPATH = "//*[@id=\"modal\"]/div/div/div[3]/div/input";
     public static final String CHECKBOX_AGREEMENT_XPATH = "//div[@class='checkbox-field']";
     public static final String BUTTON_ENTER_XPATH = "//button[@class='button enter-button normal']";
-
+    public static final String BUTTON_REGISTER_XPATH = "//button[@class='button enter-button registration normal']";
+    public static final String BUTTON_FORGOT_PASSWORD_XPATH = "//button[@class='forget-password-link']";
+    public static final String BUTTON_BACK_TO_LOGIN_XPATH = "//button[@class='goto-login link']";
+    public static final String BUTTON_CONTINUE_REGISTER_XPATH = "//button[@class='forget-password-link']";
+    public static final String BUTTON_RESEND_CODE_XPATH = "//button[@data-test='resend']";
+    public static final String BUTTON_CHANGE_PHONE_XPATH = "//button[@data-test='change-phone']";
+    public static final String BUTTON_SMS_HELP_XPATH = "//button[@data-test='help']";
 
     public static final String INVALID_LOGIN_OR_PASSWORD_XPATH = "//div[@class='error']";
+    public static final String CODE_IS_SENT_TITLE_XPATH = "//div[@class='error']";
+    public static final String SMS_HELP_TITLE_XPATH = "//div[@class='error']";
+    public static final String LOGIN_TITLE_XPATH = "//div[@class='error']";
+
 }
