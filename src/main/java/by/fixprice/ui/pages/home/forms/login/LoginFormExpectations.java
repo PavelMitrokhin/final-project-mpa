7ae39a31b0f36ed1e48c479df0afdd5428b4980e
Pavel <pamitrovel@gmail.com>
2024-12-09 20:24:12 +0300

    add INCORRECT_RESTORE_EMAIL_LIMIT

--- src/main/java/by/fixprice/ui/pages/home/forms/login/LoginFormExpectations.java
+++ src/main/java/by/fixprice/ui/pages/home/forms/login/LoginFormExpectations.java
@@ -9,5 +9,8 @@ public class LoginFormExpectations {
     public static final String PHONE_NUMBER_IS_INVALID = "Неверно указан номер телефона";
     public static final String LOGIN_TITLE = "Вход";
     public static final String SMS_HELP_TITLE = "Не приходит СМС?";
-    public static final String TOO_MANY_REQUESTS_FOR_CODE_SEND = "Слишком много запросов. Получить новый код подтверждения можно будет не ранее, чем 9 декабря 2024 в 15:30";
+    public static final String TOO_MANY_REQUESTS_FOR_CODE_SEND_FREEZE = "Слишком много запросов. Получить новый код подтверждения можно будет не ранее, чем 9 декабря 2024 в 15:30";
+    public static final String TOO_MANY_REQUESTS_FOR_CODE_SEND = "Слишком много попыток отправки кода. Повторите позднее";
+    public static final String INCORRECT_RESTORE_EMAIL = "Проверьте корректность введённых данных и попробуйте снова. Осталось попыток: 4";
+    public static final String INCORRECT_RESTORE_EMAIL_LIMIT = "Лимит попыток превышен. Повторите попытку восстановления пароля через 10 мин.";
 }
