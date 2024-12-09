eee5351ebebdb2565960cbe9fdc2a737d011f523
Pavel <pamitrovel@gmail.com>
2024-12-09 20:46:40 +0300

    add hasFiveTriesToRestore

--- src/main/java/by/fixprice/ui/pages/home/forms/login/LoginFormSteps.java
+++ src/main/java/by/fixprice/ui/pages/home/forms/login/LoginFormSteps.java
@@ -2,11 +2,18 @@ package by.fixprice.ui.pages.home.forms.login;
 
 import by.fixprice.ui.pages.home.HomePage;
 import by.fixprice.utils.UiUsers;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Objects;
+
+import static io.restassured.RestAssured.given;
 
 public class LoginFormSteps {
     HomePage homePage = new HomePage();
     LoginForm loginForm = new LoginForm();
     UiUsers user = new UiUsers();
+    private static final Logger logger = LogManager.getLogger();
 
     public String checkInvalidPhoneUser() {
         homePage.openLoginForm()
@@ -97,5 +104,33 @@ public class LoginFormSteps {
                 .clickContinueRegisterButton()
                 .clickSmsHelp();
         return loginForm.getSmsHelpTitle();
+    }
+
+    public String checkInCorrectRestoreEmail() {
+        homePage.openLoginForm()
+                .clickForgotPassword()
+                .sendLogin(user.getEmailAndPasswordUser())
+                .clickGetLink();
+        return loginForm.getErrorInvalidLoginOrPassword();
+    }
+
+    public boolean hasFiveTriesToRestore() {
+        boolean hasFiveRequestsResponse = false;
+        homePage.openLoginForm()
+                .clickForgotPassword()
+                .sendLogin(user.getEmailAndPasswordUser());
+
+        for (int i = 1; i < 6; i++) {
+            logger.info("tempt #{}, email: {}, hasFiveRequestsResponse: {}",
+                    i, user.getEmailAndPasswordUser().getLogin(), hasFiveRequestsResponse);
+            String message = loginForm.clickGetLink().getErrorInvalidLoginOrPassword();
+            logger.info("message: {}", message);
+            if (Objects.equals(message, LoginFormExpectations.INCORRECT_RESTORE_EMAIL_LIMIT)) {
+                hasFiveRequestsResponse = true;
+            }
+        }
+
+        logger.info("value \"hasFiveRequestsResponse\": {}", hasFiveRequestsResponse);
+        return hasFiveRequestsResponse;
     }
 }
