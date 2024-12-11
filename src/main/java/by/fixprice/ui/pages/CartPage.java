88778aa39e5b4988b7c5733f8d1a102de7b045ec
Pavel <pamitrovel@gmail.com>
2024-12-11 22:18:45 +0300

    add CartPage

---
+++ src/main/java/by/fixprice/ui/pages/CartPage.java
@@ -0,0 +1,12 @@
+package by.fixprice.ui.pages;
+
+import by.fixprice.ui.driver.Driver;
+import org.openqa.selenium.WebDriver;
+
+public class CartPage {
+    private WebDriver driver;
+
+    public CartPage() {
+        driver = Driver.getDriver();
+    }
+}
