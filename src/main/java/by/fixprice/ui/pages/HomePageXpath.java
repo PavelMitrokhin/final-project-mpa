2804ff9dd88be8c61bc9fb316468b7bdb0a0a6ec
Pavel <pamitrovel@gmail.com>
2024-12-11 22:00:57 +0300

    refactor LINK_HOUSEHOLD_CHEMICALS_TAB_XPATH

--- src/main/java/by/fixprice/ui/pages/HomePageXpath.java
+++ src/main/java/by/fixprice/ui/pages/HomePageXpath.java
@@ -2,5 +2,5 @@ package by.fixprice.ui.pages;
 
 public class HomePageXpath {
     public static final String BUTTON_LOGIN_FORM_XPATH = "//button[@class='log-in link']";
-    public static final String LINK_HOUSEHOLD_CHEMICALS_XPATH = "//a[@href= '/catalog/bytovaya-khimiya'][@class='details']";
+    public static final String LINK_HOUSEHOLD_CHEMICALS_TAB_XPATH = "//a[@class='link product-category nuxt-link-exact-active nuxt-link-active']";
 }
