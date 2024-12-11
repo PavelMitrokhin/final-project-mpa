38a7bae0802d6d2a858ce6e39b33a14db5bb2c8b
Pavel <pamitrovel@gmail.com>
2024-12-11 22:22:42 +0300

    add checkTotalAmountOfGoods

--- src/main/java/by/fixprice/ui/pages/CartPage.java
+++ src/main/java/by/fixprice/ui/pages/CartPage.java
@@ -1,6 +1,7 @@
 package by.fixprice.ui.pages;
 
 import by.fixprice.ui.driver.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class CartPage {
@@ -8,5 +9,9 @@ public class CartPage {
 
     public CartPage() {
         driver = Driver.getDriver();
+    }
+
+    public String checkTotalAmountOfGoods() {
+        return driver.findElement(By.xpath(CartPageXpath.TOTAL_AMOUNT_OF_GOODS_XPATH)).getText();
     }
 }
--- src/main/java/by/fixprice/ui/pages/CartPageXpath.java
+++ src/main/java/by/fixprice/ui/pages/CartPageXpath.java
@@ -1,5 +1,5 @@
 package by.fixprice.ui.pages;
 
-public class CartXpath {
+public class CartPageXpath {
     public static final String TOTAL_AMOUNT_OF_GOODS_XPATH = "//li[@class='element products']//span[@class='count']";
 }
