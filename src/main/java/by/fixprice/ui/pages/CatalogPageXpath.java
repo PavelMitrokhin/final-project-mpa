0534644ac2d14eee836316705986efc5aede5a47
Pavel <pamitrovel@gmail.com>
2024-12-11 22:12:52 +0300

    add LINK_CART_XPATH

--- src/main/java/by/fixprice/ui/pages/CatalogPageXpath.java
+++ src/main/java/by/fixprice/ui/pages/CatalogPageXpath.java
@@ -10,4 +10,5 @@ public class CatalogPageXpath {
     public static final String BUTTON_HEADER_SHOP_XPATH = "//div[@class='header-obtain-method']";
     public static final String OUTPUT_SHOP_ORDER_ADDRESS_XPATH = "//div[@class='title'][contains(text(),'г.Брест, ул.Московская, д.202')]";
     public static final String BUTTON_CLOSE_GOOD_FORM_XPATH = "//button[@class='close'][@data-test='close-button']";
+    public static final String LINK_CART_XPATH = "//a[@class='cart link']";
 }
