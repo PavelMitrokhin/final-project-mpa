d753b752f0a9e158a606f7a71e468482ccae6dfe
Pavel <pamitrovel@gmail.com>
2024-12-15 20:35:16 +0300

    refactor CartPageXpath

--- src/main/java/by/fixprice/ui/pages/cart/CartPageXpath.java
+++ src/main/java/by/fixprice/ui/pages/cart/CartPageXpath.java
@@ -1,4 +1,4 @@
-package by.fixprice.ui.pages;
+package by.fixprice.ui.pages.cart;
 
 public class CartPageXpath {
     public static final String TOTAL_AMOUNT_OF_GOODS_XPATH = "//li[@class='element products']//span[@class='count']";
