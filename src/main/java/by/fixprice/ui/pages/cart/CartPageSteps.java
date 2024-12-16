587024838b9854d63c505b9ea663aacc39796226
Pavel <pamitrovel@gmail.com>
2024-12-16 23:02:55 +0300

    add getFirstAddedGoodNameInCart

--- src/main/java/by/fixprice/ui/pages/cart/CartPageSteps.java
+++ src/main/java/by/fixprice/ui/pages/cart/CartPageSteps.java
@@ -1,10 +1,12 @@
 package by.fixprice.ui.pages.cart;
 
+import by.fixprice.ui.pages.catalog.CatalogPage;
 import by.fixprice.ui.pages.home.HomePage;
 
 public class CartPageSteps {
     HomePage homePage = new HomePage();
     CartPage cartPage = new CartPage();
+    CatalogPage catalogPage = new CatalogPage();
 
     public String getTotalAmountOfGoods() {
         homePage.openHouseholdChemicalsTab()
@@ -13,5 +15,10 @@ public class CartPageSteps {
         homePage.closeForm()
                 .clickCart();
         return cartPage.checkTotalAmountOfGoods();
+    }
+
+    public String getFirstAddedGoodNameInCart() {
+        homePage.clickCart();
+        return catalogPage.getFirstChemicalGoodName();
     }
 }
