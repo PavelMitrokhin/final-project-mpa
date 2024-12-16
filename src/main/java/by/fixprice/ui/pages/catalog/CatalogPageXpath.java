507bec1321bcf1e3e197fcb912281045998b117f
Pavel <pamitrovel@gmail.com>
2024-12-16 21:09:09 +0300

    add FIRST_CHEMICAL_GOOD_NAME_XPATH

--- src/main/java/by/fixprice/ui/pages/catalog/CatalogPageXpath.java
+++ src/main/java/by/fixprice/ui/pages/catalog/CatalogPageXpath.java
@@ -5,4 +5,5 @@ public class CatalogPageXpath {
     public static final String LINK_CART_XPATH = "//a[@class='cart link']";
     public static final String CART_BADGE_COUNTER_XPATH = "//div[@class='badge count badge-sm']";
     public static final String BUTTON_DECLINE_ALL_COOKIES_XPATH = "//button[@class='button'][contains(text(), 'Отказаться от всех cookie')]";
+    public static final String FIRST_CHEMICAL_GOOD_NAME_XPATH = "(//a[@class='title'][@data-v-56e0c2a2])[1]";
 }
