4237c46ac759dd3b2c7ba2992f3311bf2ecb1ffe
Pavel <pamitrovel@gmail.com>
2024-12-23 21:27:56 +0300

    remove extra classes

--- src/main/java/by/fixprice/ui/pages/CartPageXpath.java
+++
@@ -1,5 +0,0 @@
-package by.fixprice.ui.pages;
-
-public class CartPageXpath {
-    public static final String TOTAL_AMOUNT_OF_GOODS_XPATH = "//li[@class='element products']//span[@class='count']";
-}
--- src/main/java/by/fixprice/ui/pages/CatalogPageXpath.java
+++
@@ -1,7 +0,0 @@
-package by.fixprice.ui.pages;
-
-public class CatalogPageXpath {
-    public static final String BUTTON_ADD_FIRST_CHEMICAL_TO_CART_XPATH = "(//button[@class='button button gridProduct'])[1]";
-    public static final String LINK_CART_XPATH = "//a[@class='cart link']";
-    public static final String CART_BADGE_COUNTER_XPATH = "//div[@class='badge count badge-sm']";
-}
